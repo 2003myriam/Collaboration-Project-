c5b1b6383db074b665471e9c78bb9b9a1ca8abcb
2003myriam <lounesmyriam210@gmail.com>
2026-04-13 12:21:26 +0100

    create first page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Page1 from './Pages/Page1'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<h1>ab</h1>
-        }/>
-
-                <Route path='/page2' element={<h1>cd</h1>
-        }/>
+        <Route path='/' element={<Page1/>}/>
+        <Route path='/page2' element={<h1>cd</h1>}/>
       </Routes>
     </>
   )
